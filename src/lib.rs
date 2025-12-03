46c5d6d0d090b5aa2b0bce9844177d95f001a1c0
Patrick Lin <linpatrick11@gmail.com>
2025-12-03 13:03:52 -0500

    Implement put() log insert using (String, String) key-value and remove KvStore file field

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufWriter, BufReader};
+use std::io::{self, BufWriter, BufReader, Write};
 
 struct KvStore {
-    file: File,
     reader: BufReader<File>,
     writer: BufWriter<File>,
 }
@@ -25,6 +24,27 @@ impl KvStore {
         let reader = BufReader::new(rfile);
         let writer = BufWriter::new(wfile);
 
-        Ok(Self { file, reader, writer })
+        Ok(Self { reader, writer })
+    }
+
+    pub fn put(&mut self, key: String, value: String) -> io::Result<()> {
+        let op: u8 = 0;
+        let key_bytes = key.as_bytes();
+        let val_bytes = value.as_bytes();
+
+        let key_len = key_bytes.len() as u32;
+        let key_len_bytes = key_len.to_le_bytes();
+
+        let val_len = val_bytes.len() as u32;
+        let val_len_bytes = val_len.to_le_bytes();
+
+        self.writer.write_all(&[op])?;
+        self.writer.write_all(&key_len_bytes)?;
+        self.writer.write_all(&val_len_bytes)?;
+        self.writer.write_all(key_bytes)?;
+        self.writer.write_all(val_bytes)?;
+
+        self.writer.flush()?;
+        Ok(())
     }
 }
