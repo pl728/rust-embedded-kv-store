efbdbe2b0722582d234aa900769d95b37c74ef5c
Patrick Lin <linpatrick11@gmail.com>
2025-12-03 15:13:43 -0500

    Implement build_index() to build BTreeMap from log file, fix various implementation issues

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 use std::fs::{File, OpenOptions};
-use std::io::{self, BufWriter, BufReader, Write};
+use std::io::{self, BufWriter, BufReader, Write, Seek, Read, SeekFrom};
+use std::collections::BTreeMap;
 
-struct KvStore {
+pub struct KvStore {
     reader: BufReader<File>,
     writer: BufWriter<File>,
+    index: BTreeMap<String, u64>,
+    writer_pos: u64,
 }
 
 impl KvStore {
-    const PATH: &'static str = "data.log";
+    const PATH: &'static str = "./data.log";
 
     const OP_PUT: u8 = 0;
     const OP_DELETE: u8 = 1;
 
     pub fn new() -> io::Result<Self> {
-        let rfile = OpenOptions::new()
+        let mut rfile = OpenOptions::new()
             .read(true)
+            .write(true)
             .create(true)
             .open(Self::PATH)?;
+
+        let (index, writer_pos) = Self::build_index(&mut rfile)?;
+
+        rfile.seek(SeekFrom::Start(0))?;
         
         let wfile = OpenOptions::new()
             .write(true)
@@ -27,7 +35,57 @@ impl KvStore {
         let reader = BufReader::new(rfile);
         let writer = BufWriter::new(wfile);
 
-        Ok(Self { reader, writer })
+        Ok(Self { reader, writer, index, writer_pos })
+    }
+
+    fn build_index(file: &mut File) -> io::Result<(BTreeMap<String, u64>, u64)> {
+        let mut index = BTreeMap::new();
+        let mut offset: u64 = 0;
+
+        loop {
+            let entry_start = offset;
+            let mut op_buf = [0u8; 1];
+            match file.read_exact(&mut op_buf) {
+                Ok(()) => {}
+                Err(e) if e.kind() == io::ErrorKind::UnexpectedEof => break,
+                Err(e) => return Err(e),
+            }
+            let op = op_buf[0];
+            offset += 1;
+            
+            let mut len_buf = [0u8; 4];
+            file.read_exact(&mut len_buf)?;
+            let key_len = u32::from_le_bytes(len_buf) as u64;
+            offset += 4;
+
+            file.read_exact(&mut len_buf)?;
+            let val_len = u32::from_le_bytes(len_buf) as u64;
+            offset += 4;
+
+            let mut key_buf = vec![0u8; key_len as usize];
+            file.read_exact(&mut key_buf)?;
+            offset += key_len;
+
+            let mut val_buf = vec![0u8; val_len as usize];
+            if val_len > 0 {
+                file.read_exact(&mut val_buf)?;
+            }
+            offset += val_len;
+
+            let key = String::from_utf8(key_buf).unwrap();
+
+            match op {
+                Self::OP_PUT => {
+                    index.insert(key, entry_start);
+                },
+                Self::OP_DELETE => {
+                    index.remove(&key);
+                },
+                _ => {}
+            }
+        }
+
+        Ok((index, offset))
     }
 
     pub fn put(&mut self, key: String, value: String) -> io::Result<()> {
@@ -48,6 +106,10 @@ impl KvStore {
         self.writer.write_all(val_bytes)?;
 
         self.writer.flush()?;
+
+        self.index.insert(key, self.writer_pos);
+        self.writer_pos += 1 + 4 + 4 + key_len as u64 + val_len as u64;
+
         Ok(())
     }
 
@@ -67,6 +129,37 @@ impl KvStore {
 
         self.writer.flush()?;
 
+        self.index.remove(&key);
+        self.writer_pos += 1 + 4 + 4 + key_len as u64;
+
         Ok(())
+    }
+
+    pub fn get(&mut self, key: &str) -> io::Result<Option<String>> {
+        let Some(&offset) = self.index.get(key) else {
+            return Ok(None);
+        };
+
+        self.reader.seek(SeekFrom::Start(offset))?;
+
+        let mut op_buf = [0u8; 1];
+        self.reader.read_exact(&mut op_buf)?;
+        let _op = op_buf[0];
+
+        let mut len_buf = [0u8; 4];
+        self.reader.read_exact(&mut len_buf)?;
+        let key_len = u32::from_le_bytes(len_buf) as usize;
+        self.reader.read_exact(&mut len_buf)?;
+        let val_len = u32::from_le_bytes(len_buf) as usize;
+
+        let mut key_buf = vec![0u8; key_len];
+        let mut val_buf = vec![0u8; val_len];
+
+        self.reader.read_exact(&mut key_buf)?;
+        self.reader.read_exact(&mut val_buf)?;
+
+        let val = String::from_utf8(val_buf).unwrap();
+
+        Ok(Some(val))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use rust_embedded_kv_store::KvStore;
+use std::io::{self};
+
+fn main() -> io::Result<()> {
+    // Start from a clean slate while experimenting
+    let _ = std::fs::remove_file("data.log");
+
+    let mut store = KvStore::new()?;
+
+    println!("get(foo) before put: {:?}", store.get("foo")?);
+
+    store.put("foo".to_string(), "bar".to_string())?;
+    println!("get(foo) after put: {:?}", store.get("foo")?);
+
+    store.put("foo".to_string(), "baz".to_string())?;
+    println!("get(foo) after overwrite: {:?}", store.get("foo")?);
+
+    store.put("key2".to_string(), "value2".to_string())?;
+    println!("get(key2): {:?}", store.get("key2")?);
+
+    store.delete("foo".to_string())?;
+    println!("get(foo) after delete: {:?}", store.get("foo")?);
+
+    // Reopen to verify index rebuilding from log
+    drop(store);
+    let mut store2 = KvStore::new()?;
+    println!("get(foo) after reopen: {:?}", store2.get("foo")?);
+    println!("get(key2) after reopen: {:?}", store2.get("key2")?);
+
+    Ok(())
 }
